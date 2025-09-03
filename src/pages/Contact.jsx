07d872fa40c068e7212bf5dbcec6c89c8d4d3b07
Samson Badejo <zwitsam8@gmail.com>
2025-09-03 11:59:19 +0100

    fix: standardize component naming for FAQSection

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
-import FAQSection from "../components/FaqSection";
+import FaqSection from "../components/FaqSection";
 
 const Contact = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-900">
       <div>
         <ContactForm />
-        <FAQSection />
+        <FaqSection />
       </div>
     </div>
   );
