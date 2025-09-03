5f0b5a994ccc955e4063fba120ed5de32223f6d3
Samson Badejo <zwitsam8@gmail.com>
2025-09-03 11:36:30 +0100

    remove unused YouTube utility function

--- src/utils/youtube.js
+++
@@ -1,6 +0,0 @@
-// utils/youtube.js   (ES module)
-
-export const fetchYouTubeMeta = (url) => {
-  const id = new URL(url).searchParams.get("v");
-  return `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
-};
