48029370b416a8b281f9d64bbed1b9bf5a11a6b1
Samson Badejo <zwitsam8@gmail.com>
2025-09-09 00:02:40 +0100

    fix: adjust padding in EventList component for improved layout on different screen sizes

--- src/pages/Admin/components/AdminEvents.jsx
+++ src/pages/Admin/components/AdminEvents.jsx
@@ -118,7 +118,7 @@ export default function EventList() {
   }
 
   return (
-    <div className="px-32 py-20">
+    <div className="px-4 md:px-10 lg:px-32 py-20">
       {/* Filter + Arrows */}
       <div className="flex justify-between items-center mb-6">
         <button
